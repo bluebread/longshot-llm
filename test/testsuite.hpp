d191262d6eb802c1ab48025711ebb048a52e2a42
bluebread <hotbread70127@gmail.com>
2025-04-10 07:37:46 +0000

    change the way of printing PASS/FAILED

--- test/testsuite.hpp
+++ test/testsuite.hpp
@@ -34,12 +34,13 @@ public:
 #define GREEN_PASS   "\033[1;32m"   "PASS"    "\033[0m"
 #define RED_FAILED   "\033[1;31m"   "FAILED"  "\033[0m"
 
-#ifndef TESTER
-#error "TESTER not defined. Please define TESTER before including tests."
+#ifdef TESTER
+#define PRINT_PASS      std::cout << GREEN_PASS " - " TESTER << std::endl
+#define PRINT_FAILED    std::cout << RED_FAILED " - " TESTER << std::endl
+#else
+#define PRINT_PASS      std::cout << GREEN_PASS << std::endl
+#define PRINT_FAILED    std::cout << RED_FAILED << std::endl
 #endif
-
-#define PRINT_PASS      std::cout << GREEN_PASS " - "  << TESTER << std::endl
-#define PRINT_FAILED    std::cout << RED_FAILED " - "  << TESTER << std::endl
 
 #define TESTCASE(ANS, INPUT) \
     _testcase((ANS) == (INPUT), __FILE__, __LINE__, "(" #ANS ") == (" #INPUT ")")

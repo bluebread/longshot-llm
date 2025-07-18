0d451a3dd1b42ce408b4d7d9ebf8ca7eb36ebd5f
bluebread <hotbread70127@gmail.com>
2025-07-18 06:39:51 +0000

    neo4j example

--- rl/service/neo.py
+++ rl/service/neo.py
@@ -2,7 +2,44 @@ from neo4j import GraphDatabase
 
 # URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
 URI = "neo4j://192.168.0.65:7687"
-AUTH = ("haowei", "bread861122")
+AUTH = ("neo4j", "bread861122")
 
-with GraphDatabase.driver(URI, auth=AUTH) as driver:
-    driver.verify_connectivity()
+
+driver = GraphDatabase.driver(URI, auth=AUTH)
+session = driver.session(database="neo4j")
+
+summary = driver.execute_query("""
+    CREATE (a:Person {name: $name})
+    CREATE (b:Person {name: $friendName})
+    CREATE (a)-[:KNOWS]->(b)
+    """,
+    name="Alice", friendName="David",
+    database_="neo4j",
+).summary
+
+print("Created {nodes_created} nodes in {time} ms.".format(
+    nodes_created=summary.counters.nodes_created,
+    time=summary.result_available_after
+))
+
+records, summary, keys = driver.execute_query("""
+    MATCH (p:Person)-[:KNOWS]->(:Person)
+    RETURN p.name AS name
+    """,
+    database_="neo4j",
+)
+
+# Loop through results and do something with them
+for record in records:
+    print(record.data())  # obtain record as dict
+
+# Summary information
+print("The query `{query}` returned {records_count} records in {time} ms.".format(
+    query=summary.query, records_count=len(records),
+    time=summary.result_available_after
+))
+
+# session/driver usage
+
+session.close()
+driver.close()
